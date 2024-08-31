fac927edab71281d78f59653cc9216919a3e057f
Romuald Rousseau <romuald.rousseau@servier.com>
2024-09-01 01:10:16 +0800

    feat: Add python and java time conversion

--- shuju/src/main/java/com/github/romualdrousseau/shuju/commons/PythonSimpleDateFormat.java
+++ shuju/src/main/java/com/github/romualdrousseau/shuju/commons/PythonSimpleDateFormat.java
@@ -27,15 +27,26 @@ public class PythonSimpleDateFormat extends SimpleDateFormat {
             .replaceAll("YYYY", "%G")
             .replaceAll("yyyy", "%Y")
             .replaceAll("yy", "%y")
+            .replaceAll("y", "%-y")
             .replaceAll("MMMMM", "%B")
             .replaceAll("MMM", "%b")
             .replaceAll("MM", "%m")
+            .replaceAll("M", "%-m")
             .replaceAll("DDD", "%j")
             .replaceAll("dd", "%d")
+            .replaceAll("d", "%-d")
             .replaceAll("EEEEE", "%A")
             .replaceAll("EEE", "%a")
             .replaceAll("ww", "%W")
-            .replaceAll("u", "%u");
+            .replaceAll("u", "%u")
+            .replaceAll("HH", "%H")
+            .replaceAll("H", "%-H")
+            .replaceAll("hh", "%I")
+            .replaceAll("h", "%-I")
+            .replaceAll("mm", "%M")
+            .replaceAll("m", "%-M")
+            .replaceAll("ss", "%S")
+            .replaceAll("s", "%-S");
     }
 
     public static String toJava(final String pythonPattern) {
@@ -43,17 +54,28 @@ public class PythonSimpleDateFormat extends SimpleDateFormat {
             .replaceAll("%G", "YYYY")
             .replaceAll("%Y", "yyyy")
             .replaceAll("%y", "yy")
+            .replaceAll("%-y", "y")
             .replaceAll("%B", "MMMMM")
             .replaceAll("%b", "MMM")
             .replaceAll("%m", "MM")
+            .replaceAll("%-m", "M")
             .replaceAll("%j", "DDD")
             .replaceAll("%d", "dd")
+            .replaceAll("%-d", "d")
             .replaceAll("%A", "EEEEE")
             .replaceAll("%a", "EEE")
             .replaceAll("%W", "ww")
             .replaceAll("%w", "u")
             .replaceAll("%u", "u")
             .replaceAll("%U", "ww")
-            .replaceAll("%V", "ww");
+            .replaceAll("%V", "ww")
+            .replaceAll("%H", "HH")
+            .replaceAll("%-H", "H")
+            .replaceAll("%I", "hh")
+            .replaceAll("%-I", "h")
+            .replaceAll("%M", "mm")
+            .replaceAll("%-M", "m")
+            .replaceAll("%S", "ss")
+            .replaceAll("%-S", "s");
     }
 }
--- shuju/src/test/java/com/github/romualdrousseau/shuju/Test_Commons.java
+++ shuju/src/test/java/com/github/romualdrousseau/shuju/Test_Commons.java
@@ -19,4 +19,10 @@ public class Test_Commons {
         assertEquals("Sun,24/09/23", formatter.format(Date.from(LocalDate.of(2023, 9, 24).atStartOfDay(ZoneId.systemDefault()).toInstant())));
         assertEquals("Sun,05/12/99", formatter.format(formatter.parse("Sun,05/12/99")));
     }
+
+    @Test
+    public void testPythonSimpleDateformatWithTime() throws ParseException {
+        final PythonSimpleDateFormat formatter = new PythonSimpleDateFormat("%Y.%m.%d. %H:%M");
+        assertEquals("2023.03.16. 11:36", formatter.format(formatter.parse("2023.03.16. 11:36")));
+    }
 }
